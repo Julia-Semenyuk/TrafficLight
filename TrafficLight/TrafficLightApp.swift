dd70c0ece9f1c356a951304b7af027ff01d10018
Julia Semenyuk <julia_semenyuk@internet.ru>
2022-10-20 02:34:24 +0900

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Юлия on 20.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
